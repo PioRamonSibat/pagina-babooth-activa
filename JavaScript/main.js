72b657edacf35056c0fe0f72623d3b16bdd72350
Pio Ramon Sibat <piosebas.ramonsibat@gmail.com>
2023-06-26 19:02:15 -0300

    avanzamos pagina autobooth

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,11 +3,15 @@ const option2 = document.getElementById('option2')
 const option3 = document.getElementById('option3')
 const option4 = document.getElementById('option4')
 const option5 = document.getElementById('option5')
+const option6 = document.getElementById('option6')
+
 const content1 = document.getElementById('content1')
 const content2 = document.getElementById('content2')
 const content3 = document.getElementById('content3')
 const content4 = document.getElementById('content4')
 const content5 = document.getElementById('content5')
+const content6 = document.getElementById('content6')
+
 
 let choose = 1
 
@@ -53,6 +57,46 @@ const changeOption = () => {
         option5.classList.value = 'option',
         content5.classList.value = 'content '
     )
+    choose == 6 ? (
+        option6.classList.value = 'option option-active',
+        content6.classList.value = 'content content-active'
+    )
+    : (
+        option6.classList.value = 'option',
+        content6.classList.value = 'content '
+    )
+    choose == 7 ? (
+        option7.classList.value = 'option option-active',
+        content7.classList.value = 'content content-active'
+    )
+    : (
+        option7.classList.value = 'option',
+        content7.classList.value = 'content '
+    )
+    choose == 8 ? (
+        option8.classList.value = 'option option-active',
+        content8.classList.value = 'content content-active'
+    )
+    : (
+        option8.classList.value = 'option',
+        content8.classList.value = 'content '
+    )
+    choose == 9 ? (
+        option9.classList.value = 'option option-active',
+        content9.classList.value = 'content content-active'
+    )
+    : (
+        option9.classList.value = 'option',
+        content9.classList.value = 'content '
+    )
+    choose == 10 ? (
+        option10.classList.value = 'option option-active',
+        content10.classList.value = 'content content-active'
+    )
+    : (
+        option10.classList.value = 'option',
+        content10.classList.value = 'content '
+    )
 }
 
 option1.addEventListener('click', ()=> {
@@ -74,5 +118,25 @@ option4.addEventListener('click', ()=> {
 })
 option5.addEventListener('click', ()=> {
     choose = 5
+    changeOption()
+})
+option6.addEventListener('click', ()=> {
+    choose = 6
+    changeOption()
+})
+option7.addEventListener('click', ()=> {
+    choose = 7
+    changeOption()
+})
+option8.addEventListener('click', ()=> {
+    choose = 8
+    changeOption()
+})
+option9.addEventListener('click', ()=> {
+    choose = 9
+    changeOption()
+})
+option10.addEventListener('click', ()=> {
+    choose = 10
     changeOption()
 })
